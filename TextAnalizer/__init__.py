c985dca4668b5b36b8a6a6969788a26afbd844c0
RoboCopGay <vinicios.sousa909@gmail.com>
2019-09-24 10:23:43 -0300

    inclued new functions

--- TextAnalizer/__init__.py
+++ TextAnalizer/__init__.py
@@ -1,7 +1,15 @@
 from .dictionary import *
 
-def translate(_str, _file):
-    return Dictionary(_file).translate(_str)
+def translate(_str, _conf):
+    return Dictionary(_conf).translate(_str)
 
-def open(_file):
+def fopen(_file):
     return File(_file)
+
+def replace(_patt, _rep, _text):
+    return Dictionary().replace( _key=_patt, _rep=_rep, _text=_text)
+
+def search(_patt, _text):
+    regex = Dictionary().regex(_patt).strip()
+    res = REGEX.search( regex, _text)
+    return res.group() if res else None
