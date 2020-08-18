47395f4d849e95672b742d3309e841139852cdb5
Plankiton <pl4nk1ton@gmail.com>
2020-08-17 23:45:46 -0300

    Changing the project name

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='TextAnalyzer',
+    name='T-Rex',
     version='0.0.1',
     description='A Text Analyzer Utilities',
     author='RoboCopGay',
     author_email='vinicios.sousa909@gmail.com',
-    url='https://github.com/RoboCopGay/TextAnalyzer',
-    packages=['textanalyzer'],
+    url='https://github.com/RoboCopGay/T-Rex',
+    packages=['T-Rex'],
     long_description="A module to text management.",
     install_requires=[
         'PyYAML',
     ],
     entry_points={
         'console_scripts': [
-            'getter=textanalyzer.getter:main',
-            'translater=textanalyzer.translater:main'
+            'getter=T-Rex.get:main',
         ]
     }
 )
