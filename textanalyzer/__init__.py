91ee77aa5fd1f2542316789489dcc8e4b51dcbb3
Plankiton <pl4nk1ton@gmail.com>
2020-08-17 23:39:05 -0300

    Replace function made

--- textanalyzer/__init__.py
+++ textanalyzer/__init__.py
@@ -7,6 +7,12 @@ def get_vars(pattern, text):
 def replace(pattern, substitute, text):
     return __dictionary__.replace(pattern, substitute, text)
 
+def replace_by_vars(template, variables):
+    response = template
+    for var in variables:
+        response = variables[var].join(response.split(var))
+    return response
+
 def has_var(pattern):
     var_templates = __dictionary__.var_template.findall(pattern)
     return len(var_templates)
